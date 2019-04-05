8dc9646efd57e5dd39311ddf361e42bede207101
Luchian <luchiancosma@gmail.com>
2019-04-05 16:20:30 +0300

    Format non-minified output bundles
    Uncompressed Sass and Javascript output bundles aren't really necessary; they simply provide an overview of most project code. For this reason they should contain no comments and be formatted using the same rules as input files.

--- build/scripts/ignition.js
+++ build/scripts/ignition.js
@@ -1,49 +1,52 @@
 class Notice {
-    dismiss(element) {
-        element.classList.add("u-display-none");
-    }
+  dismiss(element) {
+    element.classList.add("u-display-none");
+  }
 }
 
 document.addEventListener("click", function(event) {
-    if (event.target.closest(".c-notice__dismiss")) {
-        event.preventDefault();
-        let notice = new Notice();
-        notice.dismiss(event.target.closest(".c-notice"));
-    }
+  if (event.target.closest(".c-notice__dismiss")) {
+    event.preventDefault();
+    let notice = new Notice();
+    notice.dismiss(event.target.closest(".c-notice"));
+  }
 });
 
 class Tabs {
-    select(element) {
-        let tabLinks = document.querySelectorAll(".c-tabs__link"), activePanelId = element.getAttribute("href"), tabPanels = document.querySelectorAll(".js-tab-panel"), activePanel = document.querySelector(activePanelId);
-        for (let i = 0; i < tabPanels.length; i++) {
-            tabLinks[i].classList.remove("is-selected");
-            tabPanels[i].setAttribute("hidden", "");
-        }
-        element.classList.add("is-selected");
-        activePanel.removeAttribute("hidden");
+  select(element) {
+    let tabLinks = document.querySelectorAll(".c-tabs__link"),
+      activePanelId = element.getAttribute("href"),
+      tabPanels = document.querySelectorAll(".js-tab-panel"),
+      activePanel = document.querySelector(activePanelId);
+    for (let i = 0; i < tabPanels.length; i++) {
+      tabLinks[i].classList.remove("is-selected");
+      tabPanels[i].setAttribute("hidden", "");
     }
+    element.classList.add("is-selected");
+    activePanel.removeAttribute("hidden");
+  }
 }
 
 document.addEventListener("click", function(event) {
-    if (event.target.closest(".c-tabs__link")) {
-        event.preventDefault();
-        let tabs = new Tabs();
-        tabs.select(event.target.closest(".c-tabs__link"));
-    }
+  if (event.target.closest(".c-tabs__link")) {
+    event.preventDefault();
+    let tabs = new Tabs();
+    tabs.select(event.target.closest(".c-tabs__link"));
+  }
 });
 
 class Tag {
-    dismiss(element) {
-        element.classList.add("u-display-none");
-    }
+  dismiss(element) {
+    element.classList.add("u-display-none");
+  }
 }
 
 document.addEventListener("click", function(event) {
-    if (event.target.closest(".c-tag__dismiss")) {
-        event.preventDefault();
-        let tag = new Tag();
-        tag.dismiss(event.target.closest(".c-tag"));
-    }
+  if (event.target.closest(".c-tag__dismiss")) {
+    event.preventDefault();
+    let tag = new Tag();
+    tag.dismiss(event.target.closest(".c-tag"));
+  }
 });
 
 export { Notice, Tabs, Tag };
