f8c8d01804b89494238771998c6b4a74c0c871dd
Luchian <luchiancosma@gmail.com>
2019-09-06 10:00:08 +0300

    Remove dismissible variant
    Dismissible tags are likely required for complex projects, but are unnecessary when providing a foundation for an interface design project.

--- build/scripts/spark.js
+++ build/scripts/spark.js
@@ -83,21 +83,3 @@ class Tabset {
 
 const tabset = new Tabset();
 tabset.handle();
-
-// TAG
-// Label items and reference content using keywords.
-// -----------------------------------------------------------------------------
-
-class Tag {
-  dismiss(element) {
-    element.classList.add("u-display-none");
-  }
-}
-
-document.addEventListener("click", function(event) {
-  if (event.target.closest(".c-tag__dismiss")) {
-    event.preventDefault();
-    let tag = new Tag();
-    tag.dismiss(event.target.closest(".c-tag"));
-  }
-});
